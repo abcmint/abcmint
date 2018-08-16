908177e0d34adf4a1deedabfffdcec3ad650821b
abcmint <gzljkj@126.com>
2018-08-16 14:46:01 +0800

    fix bug

--- src/qt/aboutdialog.cpp
+++ src/qt/aboutdialog.cpp
@@ -11,7 +11,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui->setupUi(this);
 
     // Set current copyright year
-    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; 2009-%1 ").arg(COPYRIGHT_YEAR) + tr("The  Abcmint developers"));
+    ui->copyrightLabel->setText(tr("Copyright") + QString(" &copy; %1 ").arg(COPYRIGHT_YEAR) + tr("The  Abcmint developers"));
 }
 
 void AboutDialog::setModel(ClientModel *model)
--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -19,7 +19,7 @@ SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) :
     // define text to place
     QString titleText       = QString(QApplication::applicationName()).replace(QString("-testnet"), QString(""), Qt::CaseSensitive); // cut of testnet, place it as single object further down
     QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
-    QString copyrightText   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Abcmint developers"));
+    QString copyrightText   = QChar(0xA9)+QString(" %1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Abcmint developers"));
     QString testnetAddText  = QString(tr("[testnet]")); // define text to place as single text object
 
     QString font            = "Arial";
