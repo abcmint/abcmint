f5c4d9270656d101706333f20fcebf4426dee15b
abcmint <35250375+abcmint@users.noreply.github.com>
2023-04-28 17:44:54 +0800

    Delete gpumining.h

--- gpumining.h
+++
@@ -1,7 +0,0 @@
-#ifndef ABCMINT_GPU_MINING_H
-#define ABCMINT_GPU_MINING_H
-uint64_t GPUSearchSolution(uint32_t* coefficients, unsigned int number_of_variables,
-                           unsigned int number_of_equations);
-int  GetDeviceCount();
-void SetDevice(int device);
-#endif
